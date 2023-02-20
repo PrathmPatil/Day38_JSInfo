13fa083d2d06e68a8c271330d17ef8b45ad03b2c
PrathmPatil <prathmpatil2818@gmail.com>
2023-02-20 16:09:07 +0530

    get number and find days

---
+++ ReadWriteNumber.js
@@ -0,0 +1,17 @@
+var getNumber = Math.floor(1 + Math.random() * 7);
+
+if (getNumber == 1) {
+    console.log("sunday");
+} else if (getNumber == 2) {
+    console.log("monday");
+} else if (getNumber == 3) {
+    console.log("tuesday");
+} else if (getNumber == 4) {
+    console.log("wednesday");
+} else if (getNumber == 5) {
+    console.log("thursday");
+} else if (getNumber == 6) {
+    console.log("friday");
+} else if (getNumber == 7) {
+    console.log("saturday");
+}
